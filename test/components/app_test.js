dc23ae171adc346aaccc88991292e259ae013b2a
munens <kaumbuthom@yahoo.com>
2018-02-12 19:42:48 -0800

    Sec. 2, lec. 5: A first spec

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,5 +1,5 @@
 // import helprer files off 'test_helper.js':
-import { renderComponent, expect } from './test_helper';
+import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
 // test for the app component:
@@ -8,12 +8,21 @@ import App from '../../src/components/app';
 
 // use describe to group together similar tests:
 // - pass a simple string to say what our tests are related to - to help us when our actual tests take place.
-describe('App')
+describe('App', () => {
+  
+  // whithin 'App' are all of the tests that are grouped together for 'App' or this 'spec:
+
+  // use 'it' to test a single attribute of a target:- in this case the text wihin a 'div'
+  // - 'it' is used to report how our test did when it was run. 
+  it('shows the correct text', () => {
+
+    // let us get our component:
+    const component = renderComponent(App);
+    
+    // 'expect' is used to make an assertion about a target:
+    expect(component).to.contain('React simple starter');
+
+  });
+})
 
 
-// use 'it' to test a single attribute of a target:- in this case the text wihin a 'div'
-// - 'it' is used to report how our test did when it was run.
-it('it shows the correct text')
-
-// 'expect' is used to make an assertion about a target:
-expect
