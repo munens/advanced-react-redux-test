8275132c6fcea4bea0a17fcbd5f367f3562ca596
munens <kaumbuthom@yahoo.com>
2018-02-12 18:57:29 -0800

    first commit: Sec. 2, lec. 3: Core testing - Describe, it , expect

---
+++ test/components/app_test.js
@@ -0,0 +1,19 @@
+// import helprer files off 'test_helper.js':
+import { renderComponent, expect } from './test_helper';
+import App from '../../src/components/app';
+
+// test for the app component:
+
+// test that our app component says 'Redux simple starter':
+
+// use describe to group together similar tests:
+// - pass a simple string to say what our tests are related to - to help us when our actual tests take place.
+describe('App')
+
+
+// use 'it' to test a single attribute of a target:- in this case the text wihin a 'div'
+// - 'it' is used to report how our test did when it was run.
+it('it shows the correct text')
+
+// 'expect' is used to make an assertion about a target:
+expect
